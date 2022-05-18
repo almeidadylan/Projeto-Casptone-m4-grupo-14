d28d9c88caf43f4f334383ac7ea3d20864c5903e
Lorenzo Marques <lorenzo.marques246@gmail.com>
2022-05-18 15:42:52 -0300

    fix:foreignKey agora sao adicionadas a tabela musics

---
+++ src/migrations/1652899037441-addRefToMusicsTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from "typeorm";
+
+export class AddReferencesOnOrderProductsTable1652100842776
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      "musics",
+      new TableForeignKey({
+        name: "UsersFK",
+        columnNames: ["id_user"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "users",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "musics",
+      new TableForeignKey({
+        name: "CategoriesFK",
+        columnNames: ["id_category"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "categories",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey("musics", "UsersFK");
+    await queryRunner.dropForeignKey("musics", "CategoriesFK");
+  }
+}

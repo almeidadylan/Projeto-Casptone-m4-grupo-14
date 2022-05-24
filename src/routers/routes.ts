4c684c71daa78f11b8f0fd9af509afbd00999883
Lorenzo Marques <lorenzo.marques246@gmail.com>
2022-05-24 14:49:35 -0300

    FEAT:jwtAuthMiddleware

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
+import tokenAuth from "../middlewares/tokenAuth";
 
 import createCategoryController from "../controllers/categories/createCategory.controller";
 import deleteCategoryController from "../controllers/categories/deleteCategory.controller";
@@ -8,17 +9,20 @@ import listCategoriesByNameOrIdController from "../controllers/categories/listCa
 import uploadMusicController from "../controllers/musics/uploadMusic.controller";
 import userCreateController from "../controllers/users/userCreate.controller";
 import userLoginController from "../controllers/users/userLogin.controller";
-const multerConfig = require("../config/multer")
-
+const multerConfig = require("../config/multer");
 
 const routes = Router();
 
 routes.post("/users", userCreateController);
 routes.post("/users/login", userLoginController);
 routes.post("/categories", createCategoryController);
-routes.delete("/categories/:id", deleteCategoryController);
+routes.delete("/categories/:id", tokenAuth, deleteCategoryController);
 routes.get("/categories", listCategoriesController);
 routes.get("/categories/:name_id", listCategoriesByNameOrIdController);
-routes.post("/music", multer(multerConfig).single("file"), uploadMusicController)
+routes.post(
+  "/music",
+  multer(multerConfig).single("file"),
+  uploadMusicController
+);
 
 export default routes;
